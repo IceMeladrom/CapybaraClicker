6a139df6f464f0432841bad1d6d1b34fdd4512b4
Robert Dzhioev <robertkryt@gmail.com>
2024-10-23 01:53:43 +0300

    temp

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
-root = ttk.Window(themename="vapor")
 
-b1 = ttk.Button(root, text="Submit", bootstyle="success")
-b1.pack(side=LEFT, padx=5, pady=10)
+class Game:
+    def __init__(self):
+        self.clicks = 0
+        self.isPressed = False
 
-b2 = ttk.Button(root, text="Submit", bootstyle="info-outline")
-b2.pack(side=LEFT, padx=5, pady=10)
+        self.root = ttk.Window(themename="solar")
 
-root.mainloop()
+        self.capybara1 = ttk.PhotoImage(file='static/capybara1.png')
+        self.capybara2 = ttk.PhotoImage(file='static/capybara2.png')
+        self.capybara3 = ttk.PhotoImage(file='static/capybara3.png')
+        self.capybara4 = ttk.PhotoImage(file='static/capybara4.png')
+
+        self.capybaraMainImg = self.capybara1
+
+        self.capybaraButton = ttk.Button(self.root, text="Submit", image=self.capybaraMainImg, bootstyle=OUTLINE,
+                                         command=self.click)
+        self.capybaraButton.bind('<ButtonPress-1>', self.btnPress)
+        self.capybaraButton.bind('<ButtonRelease-1>', self.btnUnpress)
+        self.capybaraButton.pack(side=LEFT, padx=5, pady=10)
+
+        self.text = ttk.Label(self.root, text="Hola!")
+        self.text.pack(side=BOTTOM, padx=5, pady=10)
+
+        self.root.mainloop()
+
+    def click(self):
+        self.clicks += 1
+        self.text['text'] = f'{self.clicks} capybara coins'
+
+    def btnPress(self, event):
+        self.isPressed = True
+        if self.capybaraMainImg == self.capybara1:
+            self.capybaraMainImg = self.capybara2
+        else:
+            self.capybaraMainImg = self.capybara4
+
+    def btnUnpress(self, event):
+        self.isPressed = False
+        if self.capybaraMainImg == self.capybara2:
+            self.capybaraMainImg = self.capybara1
+        else:
+            self.capybaraMainImg = self.capybara3
+
+
+if __name__ == '__main__':
+    game = Game()
